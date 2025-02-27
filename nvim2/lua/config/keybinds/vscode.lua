e8f148a9ab535ef42c4298a61a04323d201b1ce0
decode390 <decode390@gmail.com>
2025-02-27 16:27:52 -0500

    run test vscode keybind

--- nvim2/lua/config/keybinds/vscode.lua
+++ nvim2/lua/config/keybinds/vscode.lua
@@ -12,3 +12,20 @@ mapVscode('za', 'editor.toggleFold')
 mapVscode('<leader>e', 'workbench.view.explorer')
 
 mapVscode('<leader>q', 'workbench.action.closeActiveEditor')
+
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*",
+	callback = function() 
+		local fileType = vim.bo.filetype
+		
+		if fileType == "cs" then
+			mapVscode('<leader>tr', 'dotnet.test.runTestsInContext')
+		else
+			local existing_map = vim.fn.maparg('<leader>tr', 'n')
+			if existing_map ~= "" then
+				vim.keymap.del('n', '<leader>tr')
+			end
+		end
+	end
+})
